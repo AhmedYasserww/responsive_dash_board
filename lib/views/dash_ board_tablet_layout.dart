25a78cd56d729ec11d469d87f48b38fbb8039140
AhmedYasserww <ahmedyasser2105ww@gmail.com>
2024-11-26 00:00:12 +0200

    solve some issues in the desktop layout

--- lib/views/dash_ board_tablet_layout.dart
+++ lib/views/dash_ board_tablet_layout.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_ui_dash_board/views/dash_board_mobile_layout.dart';
-import 'package:responsive_ui_dash_board/views/widgets/all_expenses_and_quick_invoice_sec.dart';
 import 'package:responsive_ui_dash_board/views/widgets/custom_drawer.dart';
-import 'package:responsive_ui_dash_board/views/widgets/inCome_section/in_come_section.dart';
-import 'package:responsive_ui_dash_board/views/widgets/myCard_and_transaction_histroySection.dart';
 class DashBoardTabletLayout extends StatelessWidget {
   const DashBoardTabletLayout({super.key});
 
